82074edb3ef2fe999d5a1921203340797c209bae
Eduardo Pitz <pitzeduardo@gmail.com>
2019-05-29 00:03:59 -0300

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,20 +1,21 @@
+import copy
 
 # Para um grafo ter um Caminho Euleriano, é suficiente e necessário satisfazer uma de duas condições:
 #  - Todos os vértices do grafo tem que ter grau par.
 #  - Todos os vértices (ignorando-se os de grau zero) tem grau par, exceto dois vértices que possuem grau ímpar.
+# https://repl.it/repls/AnxiousDarkmagentaComputergraphics
 
 if __name__=='__main__':
     
     grafo = {
-        '1' : ['1','3','5'],
-        '2' : ['4','5'],
-        '3' : ['1','5'],
-        '4' : ['5','2'],
-        '5' : ['1','2','3']
+        0 : [1,3],
+        1 : [2,0],
+        2 : [3,1],
+        3 : [2,0]
     }
-    arestasVisitadas = []
-    print('Grafo gerado: ',grafo)
-
+    arestasVisitadas     = []
+    caminhoEulerVisitado = []
+    
     def algoritmoDFS(grafo, aresta, arestasVisitadas):
         if (aresta not in arestasVisitadas):
             arestasVisitadas.append(aresta)
@@ -47,12 +48,63 @@ if __name__=='__main__':
                 if (verticesComGrauImpar > 2):
                     return False
         return True
+  
+    def printCaminhoEuleriano(grafo):
+        caminhoEulerVisitado = []
 
-    grafoPossuiCaminhoEuleriano = euleriano(grafo)
+        index = 0
+        for aresta in grafo:
+            grau = grauAresta(grafo, aresta)
+            
+            if (grau % 2 != 0):
+                index = aresta
+                break
+        printarCaminhoEuleriano(grafo, index)
+    
 
-    if (grafoPossuiCaminhoEuleriano):
-        camihoEuleriano = algoritmoDFS(grafo, '2', arestasVisitadas)
-        print('O grafo gerado possui caminho euleriano: ')
-        print(camihoEuleriano)
+    def printarCaminhoEuleriano(grafo, index):
+        cloneGrafo = copy.deepcopy(grafo)
+        
+        for v in cloneGrafo[index]:
+            if validarProximoPasso(grafo, index, v):
+                if (index not in caminhoEulerVisitado):
+                    print('[>>]', index)
+                    caminhoEulerVisitado.append(index)
+                    grafo = removerCaminho(grafo, index, v)
+                    printarCaminhoEuleriano(grafo, v)
+
+
+    def removerCaminho(grafo, u, v):
+        for index, key in enumerate(grafo[u]):
+            if key == v:
+                grafo[u].pop(index)
+        for index, key in enumerate(grafo[v]):
+            if key == u:
+                grafo[v].pop(index)
+        return grafo
+
+    def validarProximoPasso(grafo, index, v):
+        if len(grafo[index]) == 1:
+            return True
+        else:
+            # print('index:', index) 
+            countA = algoritmoDFS(grafo, index, [])
+            grafo  = removerCaminho(grafo, index, v)
+            countB = algoritmoDFS(grafo, v, [])            
+            
+            if (len(countA) > len(countB)):
+                return False
+            return True
+
+    grafoConexo = algoritmoDFS(grafo, 0, arestasVisitadas)
+    if (not grafoConexo):
+        print('O grafo não é conexo.')
     else:
-        print('O grafo gerado NÃO possui caminho euleriano')
+        print('O grafo é conexo.')
+
+        grafoPossuiCaminhoEuleriano = euleriano(grafo)
+        if (grafoPossuiCaminhoEuleriano):
+            print('O grafo gerado possui caminho euleriano.')
+            printCaminhoEuleriano(grafo)
+        else:
+            print('O grafo gerado NÃO possui caminho euleriano.')
